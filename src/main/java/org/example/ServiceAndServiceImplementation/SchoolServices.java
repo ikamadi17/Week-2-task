978304529daa62932b434ba53011c4d8ef48d2be
decadev Ikebobo <ikennaamadi17@gmail.com>
2023-10-24 00:26:46 -0700

    Create Repo.repository

---
+++ src/main/java/org/example/ServiceAndServiceImplementation/SchoolServices.java
@@ -0,0 +1,6 @@
+package org.example.ServiceAndServiceImplementation;
+
+import java.util.
+public interface PrincipalService {
+
+}
---
+++ src/main/java/org/example/entities/Course.java
@@ -0,0 +1,11 @@
+package org.example.entities;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Subject {
+    public static <Arraylist> void main(String[] args) {
+        ArrayList<String> Arraylist = new ArrayList<>();
+        Arraylist<String> = new ArrayList<>(Arrays.asList("MATHEMATICS", "ENGLISH", "PHYSICS", "CHEMISTRY", "BIOLOGY", "ECONOMICS"));
+    }
+}
